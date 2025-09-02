c261eccd2f980b462ed8d488704495340e288eb3
Thaina <thaina.lima47@etec.sp.gov.br>
2025-09-02 13:55:45 -0300

    teste 1

---
+++ appIcone/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>oie</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
