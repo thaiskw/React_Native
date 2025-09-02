e562e7ae83a4e47b2b5fc15bb21fce1773d1f993
Thaina <thaina.lima47@etec.sp.gov.br>
2025-09-02 14:31:44 -0300

    implementação de icones

--- appIcone/App.js
+++ appIcone/App.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import { StyleSheet, Text, View, TouchableOpacity, TextComponent, } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 export default function App() {
+
+  const mostrarMensagem = () => {
+    alert('Botão pressionado!')
+  };
+
   return (
     <View style={styles.container}>
-      <Text>oie</Text>
+      <Text style={styles.TextComponent}>Bem-Vindo!</Text>
+      <TouchableOpacity onPress={mostrarMensagem} style={styles.btn}>
+        <AntDesign name="check" size={30} color='black'></AntDesign>
+        <Text style={styles.btnTexto}>Pressione aqui</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#262626',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: 'pink',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 5,
+    margin: 20,
+    borderWidth: 3,
+    borderColor: 'purple',
+    borderRadius: 10,
+  },
+  btnTexto: {
+    fontSize: 20,
+    color: 'black',
+    marginLeft: 10,
+    fontWeight: 'bold',
+  },
+  TextComponent: {
+    fontSize: 45,
+    color: 'white',
+    marginBottom: 150,
   },
 });
