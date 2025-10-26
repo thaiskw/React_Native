c44eba358f41491468a9181864a577c10ed15b05
thaikw <thaina.lima47@etec.sp.gov.br>
2025-10-25 21:10:25 -0300

    pasta web

--- appBiscoito/src/screens/Integrantes.js
+++ appBiscoito/src/screens/Integrantes.js
@@ -24,7 +24,7 @@ const estilos = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 21,
+    padding: 20,
   },
   titulo: {
     fontSize: 24,
